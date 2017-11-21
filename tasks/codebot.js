1f71bad78fd52bf074b436f9683404a374a2550f
Delmo <delmosaurio@gmail.com>
2017-11-21 15:15:07 +0000

    implement codebot:client

--- tasks/codebot.js
+++ tasks/codebot.js
@@ -7,11 +7,19 @@ export default (gulp, plugins, config) => {
 	let clientModules = [
 		path.join(config.root, 'templates/client/route')
 	];
+	
+	task_('codebot:app', 'templates/app.model.json', clientModules, 'client/');
+	task_('codebot:curador', 'templates/curador.model.json', clientModules, 'client/');
+	task_('codebot:social', 'templates/social.model.json', clientModules, 'client/');
 
-	gulp.task('codebot:app', () => {
-		return gulp.src(path.join(config.root, 'templates/app.model.json'))
-							 .pipe(plugins.codebot({ modules: clientModules, output: path.join(config.root, 'client/') }))
-							 .pipe(gulp.dest(config.root));
-	});
+	gulp.task('codebot:client', ['codebot:app', 'codebot:curador', 'codebot:social']);
+
+	function task_(name, model, modules, relOutput) {
+		gulp.task(name, () => {
+			return gulp.src(path.join(config.root, model))
+								 .pipe(plugins.codebot({ modules: modules, output: path.join(config.root, relOutput) }))
+								 .pipe(gulp.dest(config.root));
+		});
+	}
 
 }
