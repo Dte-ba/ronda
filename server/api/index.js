1849743d36a8e6f5e2a2616c79361d4e9bce60f7
Delmo <delmosaurio@gmail.com>
2017-11-24 16:03:27 +0000

    create a robust server

--- server/api/index.js
+++ server/api/index.js
@@ -1,20 +1,9 @@
 'use strict';
 
 var express = require('express');
-var writer = require('./writer');
-var qutils = require('./query-utils');
-var mock = require('./mock');
 
 var router = express.Router();
 
-router.use('/mock', qutils([]), mock);
-
-router.use(writer());
-
-router.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
+router.use('/users', require('./user'));
 
 module.exports = router;
