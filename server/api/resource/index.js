42f9921e79cd1c0af91c07ec098dd2b44e17d18a
Delmo <delmosaurio@gmail.com>
2018-04-09 16:43:13 -0300

    fix security things

--- server/api/resource/index.js
+++ server/api/resource/index.js
@@ -7,10 +7,10 @@ import querymen from 'querymen';
 
 var router = new Router();
 
-router.get('/', auth.isAuthenticated(), querymen.middleware(), controller.index);
+router.get('/', auth.hasRole('curador'), querymen.middleware(), controller.index);
 router.delete('/:id', auth.hasRole('curador'), controller.destroy);
 router.put('/:id', auth.hasRole('curador'), controller.update);
-router.get('/:id', auth.isAuthenticated(), controller.show);
+router.get('/:id', auth.hasRole('curador'), controller.show);
 router.post('/:id/publish', auth.hasRole('curador'), controller.publish);
 router.post('/', auth.hasRole('curador'), controller.create);
 
