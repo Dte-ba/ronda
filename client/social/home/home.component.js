7c0dc29b45830d5f7d564c72bb31a1bffaf450c1
Delmo <delmosaurio@gmail.com>
2018-04-12 15:56:16 -0300

    making banner responsive

--- client/social/home/home.component.js
+++ client/social/home/home.component.js
@@ -23,36 +23,42 @@ export default class HomeComponent extends SocialComponent {
     this.sectionName = this.$stateParams.seccion || 'home';
     let sections = {
       'home': {
-        caption:  '<h1>Un entorno de educación accesible</h1> <p>para compartir prácticas de enseñanza inclusivas pensadas desde la diversidad</p>',
+        caption:  '<h1><span class="font-primary-regular">Un entorno</span><br/> de educación accesible</h1> <p>para compartir prácticas de enseñanza inclusivas pensadas desde la diversidad</p>',
+        image: '/assets/img/banner/index_image.png',
         title: 'Bienvenido',
         description: 'Ronda es un entorno de educación accesible para compartir prácticas de enseñanza inclusivas pensadas desde la diversidad',
       },
       'propuestas': {
-        caption:  '<h1>Propuestas <br /><small>pedagógicas</small></h1> <p>Dinámicas, flexibles y transversales. Para diseñar tu clase  integrando  nuevas experiencias de aprendizaje.</p>',
+        caption:  '<h1><span class="font-primary-regular">Propuestas</span><br/>pedagógicas</h1> <p>Dinámicas, flexibles y transversales. Para diseñar tu clase  integrando  nuevas experiencias de aprendizaje.</p>',
+        image: '/assets/img/banner/propuesta_image.png',
         title: 'Propuestas pedagógicas',
         description: 'Propuestas pedagógicas Dinámicas, flexibles y transversales. Para diseñar tu clase  integrando  nuevas experiencias de aprendizaje.',
         type: 'propuesta'
       },
       'actividades': {
         caption:  '<h1>_Actividades <br />accesibles</h1> <p>Diseñadas  para  participar , interactuar y aprender  en el aula  inclusiva.</p>',
+        image: '/assets/img/banner/actividades_image.png',
         title: 'Actividades accesibles',
         description: 'Actividades accesibles diseñadas  para  participar , interactuar y aprender  en el aula  inclusiva.',
         type: 'actividad'
       },
       'herramientas': {
         caption:  '<h1>Herramientas_</h1> <p>Software para crear actividades, rampas digitales y entornos editables.</p>',
+        image: '/assets/img/banner/herramientas_image.png',
         title: 'Herramientas',
         description: 'Software para crear actividades, rampas digitales y entornos editables.',
         type: 'herramienta'
       },
       'orientaciones': {
         caption:  '<h1>_Orientaciones</h1> <p>Con tutoriales, documentos y sitios de interés  que sirven de apoyo a tus prácticas de enseñanza.</p>',
+        image: '/assets/img/banner/orientaciones_image.png',
         title: 'Orientaciones',
         description: 'Orientaciones con tutoriales y documentación que sirven de apoyo para tus prácticas.',
         type: 'orientacion'
       },
       'mediateca': {
         caption: '<h1>Mediateca_</h1> <p>Recursos didácticos para mirar, leer y escuchar.</p>',
+        image: '/assets/img/banner/mediateca_image.png',
         title: 'Mediateca',
         description: 'Recursos didácticos para mirar, leer y escuchar.',
         type: 'mediateca'
