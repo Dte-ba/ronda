16eabf86778ab651d2bb855cedc156569279276a
Delmo <delmosaurio@gmail.com>
2018-04-16 15:19:24 -0300

    add autoridades

--- client/social/institucional/institucional.component.js
+++ client/social/institucional/institucional.component.js
@@ -11,6 +11,17 @@ export default class InstitucionalComponent extends SocialComponent {
 
     this.coordinador = 'Daniel Giganti';
 
+    this.teamBA = [
+      { cargo: 'Gobernadora', name: 'Lic. María Eugenia Vidal' },
+      { cargo: 'Vicegobernador ', name: 'Dr. Daniel Salvador' },
+      { cargo: 'Director General de Cultura y Educación ', name: 'Lic. Gabriel Sanchez Zinny' },
+      { cargo: 'Vicepresidente 1º del Consejo General de Cultura y Educación ', name: 'Dr. Claudio Crissio' },
+      { cargo: 'Jefe de Gabinete de Asesores ', name: 'Don Javier Mezzamico' },
+      { cargo: 'Subsecretario de Educación ', name: 'Lic. Sergio Siciliano' },
+      { cargo: 'Subsecretaria de Políticas Docentes y Gestión Territorial ', name: 'Dra. Florencia Castro' },
+      { cargo: 'Directora de Innovación y Tecnología Educativa ', name: 'Prof. Liliana Vigolo' },
+    ];
+
     this.teamPedagogico = [
       'Gabriela Sanguintetti ',
       'Patricia Valenzuela',
